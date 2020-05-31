82ac172112357a7c1645254799ec40fc65471d91
Manuel Manfredi <manumanfro95@gmail.com>
2020-05-31 20:05:06 +0200

    Update class Player.java

--- src/main/java/StrategyCommander/model/Player.java
+++ src/main/java/StrategyCommander/model/Player.java
@@ -1,4 +1,30 @@
 package StrategyCommander.model;
 
+import javax.persistence.*;
+
+/**
+ * Classe che rappresenta il giocatore prensente nel torneo
+ */
+@Entity
 public class Player {
+
+    /**
+     * elenco di variabili che caratterizzano l'oggetto Player
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Long id; //id del giocatore
+
+    @Column(nullable = false)
+    private String name; //nome del giocatore
+
+    @Column(nullable = false)
+    private int point; //punti del giocatore
+
+    /**
+     * Costruttore vuoto sempre comodo
+     */
+    public Player() {
+    }
 }
