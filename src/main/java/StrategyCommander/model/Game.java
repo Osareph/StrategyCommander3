46c2c92e2367d99930bd069c9c38b8ec342d5535
Manuel Manfredi <manumanfro95@gmail.com>
2020-06-04 14:37:47 +0200

    Aggiunti getter e setter nella classe Game insieme ai costruttori. aggiunti commenti utili

--- src/main/java/StrategyCommander/model/Game.java
+++ src/main/java/StrategyCommander/model/Game.java
@@ -15,7 +15,59 @@ public class Game {
     @Column(nullable = false)
     private Long id; //id del gioco
 
+    /**
+     * Elenco dei giocatori che partecipano al torneo
+     */
     @OneToMany(mappedBy = "game")
     private List<Player> players;//lista dei giocatori presenti
 
+    //GETTER & SETTER
+
+    /**
+     * getId
+     * @return id del torneo
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * setId modifica l'id del torneo
+     * @param id identificatore del torneo
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * getPlayers
+     * @return elenco dei giocatori presenti nel torneo
+     */
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    /**
+     * setPlayers modifica l'elenco dei giocatori presenti nel torneo
+     * @param players elenco dei giocatori nel torneo
+     */
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    //COSTRUTTORI
+
+    /**
+     * Costruttore Vuoto
+     */
+    public Game(){
+    }
+
+    /**
+     * Costruttore
+     * @param id identificatore della partita
+     */
+    public Game(Long id){
+        this.id = id;
+    }
 }
