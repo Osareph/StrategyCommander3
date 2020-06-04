faf0a956ae1c359d4e5a38b74369a0f07dc1e72c
Manuel Manfredi <manumanfro95@gmail.com>
2020-06-04 14:08:19 +0200

    aggiunti Getter e Setter per le variabili della classe Turn insieme al costruttore. aggiunti commenti utili

--- src/main/java/StrategyCommander/model/Turn.java
+++ src/main/java/StrategyCommander/model/Turn.java
@@ -18,6 +18,59 @@ public class Turn {
     @Column(nullable = false)
     private Long id; //id del turno
 
+    /**
+     * elenco dei tavoli presenti nel turno
+     */
     @OneToMany(mappedBy = "turn")
     private List<Table> tables; //elenco dei tavoli durante il turno
+
+    //GETTER & SETTER
+
+    /**
+     * getId
+     * @return id del turno
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * setId modifica l'id del turno
+     * @param id identificatore del turno
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * getTables
+     * @return elenco dei tavoli presenti nel turno
+     */
+    public List<Table> getTables() {
+        return tables;
+    }
+
+    /**
+     * setTable modifica l'elenco dei tavoli nel turno
+     * @param tables elenco tavoli del turno
+     */
+    public void setTables(List<Table> tables) {
+        this.tables = tables;
+    }
+
+    //COSTRUTTORI
+
+    /**
+     * Costruttore vuoto
+     */
+    public Turn(){
+    }
+
+    /**
+     * Costruttore
+     * @param id identificatore del tavolo
+     */
+    public Turn(Long id) {
+        this.id = id;
+    }
 }
