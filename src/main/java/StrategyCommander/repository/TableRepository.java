21db1fc1eeb6d1c84e98660491d572e76ac09431
Manuel Manfredi <manumanfro95@gmail.com>
2020-06-09 15:39:52 +0200

    Implementazioni di metodi per la gestione della repository del tavolo

--- src/main/java/StrategyCommander/repository/TableRepository.java
+++ src/main/java/StrategyCommander/repository/TableRepository.java
@@ -1,7 +1,10 @@
 package StrategyCommander.repository;
 
 import StrategyCommander.model.Table;
+import StrategyCommander.model.Turn;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  *Interfaccia che aiuta l'inizzializzazione della repository del Tavolo
@@ -14,4 +17,23 @@ public interface TableRepository extends CrudRepository<Table, Long> {
      * @return ritorna il tavolo da gioco che ha il numero indicato
      */
     public Table findByNumber(int number);
+
+    /**
+     * Elimina il tavolo da gioco con l'Id indicato
+     * @param id identificatore del tavolo da cancellare
+     */
+    public void deleteTableById(Long id);
+
+    /**
+     * ricerca l'elenco dei tavoli che fanno parte dello stesso turno
+     * @param turn turno in cui sono presenti i tavoli richiesti
+     * @return elenco dei tavoli che partecipano al turno
+     */
+    public List<Table> findAllByTurn(Turn turn);
+
+    /**
+     * Elimina tutti i tavoli all'interno di un singolo turno
+     * @param turn turno di cui i tavoli vanno cancellati
+     */
+    public void deleteAllByTurn(Turn turn);
 }
